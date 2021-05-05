48fea47367839333de47f98795f0a6d713b6d286
Henry <hankgantner@gmail.com>
2021-05-05 10:22:06 -0500

    working backend

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import uuid from 'uuid';
-
+import { v4 as uuidv4 } from 'uuid';
 var moodIntances = [];
 
 const app = express();
@@ -32,8 +31,11 @@ app.post('/:username', (req, res) => {
     var insertMood = req.body;
     if(insertMood) {
         delete insertMood.showTimeline;
-        delete insertMood.history;        
+        delete insertMood.history;       
+        insertMood.id =  uuidv4();
         moodIntances.push(insertMood);
+
+        console.log(insertMood);
         res.status(200).json(insertMood);
     } else {
         res.status(400).json({ message: 'Request body should be a mood' });
